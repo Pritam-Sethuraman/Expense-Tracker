ec0242654e8933f93f1d0675c957b494ea954973
Pritam Sethuraman <pritamsethuraman@gmail.com>
2024-05-01 14:47:27 -0400

    Basic template ready

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,26 @@
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpense />
+    <TransactionHistory />
+    <AddTransaction />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpense.vue";
+import TransactionHistory from "./components/TransactionHistory.vue";
+import AddTransaction from "./components/AddTransaction.vue";
 
 export default {
   components: {
     Header,
     Balance,
+    IncomeExpense,
+    TransactionHistory,
+    AddTransaction,
   },
 };
 </script>
